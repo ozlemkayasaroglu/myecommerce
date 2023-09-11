926ef8aad36110fd3ef06a663b558339898edad5
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-09-11 15:15:42 +0300

    get ve put işlemleri

--- app/users/[id]/edit/page.js
+++ app/users/[id]/edit/page.js
@@ -1,78 +1,133 @@
-import { useState } from 'react';
+"use client";
+import { useEffect, useState } from "react";
+import UserData from "@/components/UserData";
 
-export default function Edit({ params }) {
-  const [data, setData] = useState({
-    id: '',
-    firstName: '',
-    lastName: '',
-    // Diğer alanları da ekleyin
-  });
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setData({ ...data, [name]: value });
-  };
 
 
 
-export default async function Edit({ params }) {
+export default function Edit({ params }) {
+  const userId = params.id;
 
-  const responseData = await fetch(
-    `http://localhost:3001/users?id=${params.id}`
-  );
-  const data = await responseData.json();
-
- 
-  const updateUserData = async () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
     try {
-      const response = await fetch(`http://localhost:3001/users/${userData.id}`, {
+      const response = await fetch(`/users?id=${userId}`, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(userData),
       });
-
+  
       if (!response.ok) {
-        throw new Error('Güncelleme işlemi başarısız oldu.');
+        throw new Error('Verileri güncellerken bir hata oluştu.');
       }
-
-      console.log('Kullanıcı başarıyla güncellendi.');
+  
+      console.log('Veriler başarıyla güncellendi');
     } catch (error) {
-      console.error('Hata:', error.message);
+      console.error(error.message);
     }
   };
 
+  const [userData, setUserData] = useState({
+    id: '',
+    image: '',
+    firstName: '',
+    lastName:'',
+    userName:'',
+    phone: '',
+    age:'',
+    email:'',
+    address: {
+      address: '',
+      city:'',
+    },
+    company: {
+      address: {
+        address: '',
+        city: '',
+      },
+    },
+
+  });
+
+  
+  
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+
+  if (name in userData) {
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  } else {
+    
+    const [parent, child] = name.split('.');
+    setUserData({
+      ...userData,
+      [parent]: {
+        ...userData[parent],
+        [child]: value,
+      },
+    });
+  }
+  console.log(userData);
+};
+ 
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await UserData(params.id)
+      setUserData(data[0])
+    }
+    fetchData()
+  }, [userId]);
+
+  
   
 
   return (
     <div>
       <h1>Kullanıcı Düzenleme Sayfası</h1>
-
+      {userData ?
       <form>
         <label>Kullanıcı Id </label>
-        <input  onChange={handleInputChange} value={data[0].id}></input>
+        <input  onChange={handleInputChange} value={userData.id}></input><br />
+
         <label>Profil Fotoğrafı:</label>
-        <img src={data[0].image} alt="Profil Fotoğrafı" />
+        <img src={userData.image} alt="Profil Fotoğrafı" /><br />
 
         <label>First Name: </label>
-        <input  onChange={handleInputChange}  value={data[0].firstName}></input>
+        <input  onChange={handleInputChange}  value={userData.firstName}></input>
+        <br />
         <label>Last Name: </label>
-        <input  onChange={handleInputChange} value={data[0].lastName}></input>
+        <input  onChange={handleInputChange} value={userData.lastName}></input>
+        <br />
         <label>Kullanıcı Adı: </label>
-        <input  onChange={handleInputChange} value={data[0].username}></input>
+        <input  onChange={handleInputChange} value={userData.username}></input>
+        <br />
         <label>Telefon Numarası: </label>
-        <input onChange={handleInputChange} value={data[0].phone}></input>
+        <input onChange={handleInputChange} value={userData.phone}></input>
+        <br />
         <label>Age: </label>
-        <input  onChange={handleInputChange} value={data[0].age}></input>
+        <input  onChange={handleInputChange} value={userData.age}></input>
+        <br />
         <label>Email </label>
-        <input  onChange={handleInputChange} value={data[0].email}></input>
+        <input  onChange={handleInputChange} value={userData.email}></input>
+        <br />
         <label>Adres: </label>
-        <input  onChange={handleInputChange} value={data[0].address.address + " /"}></input>
+        <input  onChange={handleInputChange} value={userData.address.address + " /"}></input>
+        <br />
         <label>İş Adres: </label>
-        <input  onChange={handleInputChange} value={data[0].company.address.address + " /"}></input>
-        <button>Güncelle</button>
+        <input  onChange={handleInputChange} value={userData.company.address.address + " /"}></input>
+        <br />
+        <button onClick={handleSubmit}>Güncelle</button>
       </form>
+    :
+      <div>Loading</div>
+    }
     </div>
   );
 }
---
+++ components/UserData.js
@@ -0,0 +1,11 @@
+export default async function UserData(id) {
+    const res = await fetch(
+        `http://localhost:3001/users?id=${id}`
+      );
+  
+   if (!res.ok) {
+     throw new Error('Failed to fetch data')
+   }
+   const userData = await res.json()
+   return userData;
+}
