d91a061784f885af044398f749d10649c44713a0
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-10-06 16:23:20 +0300

    users ui

--- app/users/page.js
+++ app/users/page.js
@@ -26,9 +26,14 @@ export default function UserList() {
   }, []);
 
   return (
-    <div>
-      <h1>Kullanıcı Listesi</h1>
-
+    <div className="container mx-auto p-5 bg-slate-100 border-slate-300 rounded m-4">
+      <div className="flex space-x-4 bg-amber-400 ">
+        <h1 className="space-x-4 bg-amber-400 hover:bg-amber-300 p-5">
+          <p className="text-white text-m block uppercase tracking-wide font-bold">
+            Ürün Görüntüleme Sayfası
+          </p>
+        </h1>
+      </div>
       {loading ? (
         <p>Veriler yükleniyor...</p>
       ) : (
