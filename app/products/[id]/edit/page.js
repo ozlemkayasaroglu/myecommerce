3f0dbcaba056fcd5ae89044f9a3ea7bd6bbdb498
[teamdeathmatch] <[ozlemkayasaroglu@gmail.com]>
2023-10-06 15:29:11 +0300

    product sayfaları tamamlanmıştır

--- app/products/[id]/edit/page.js
+++ app/products/[id]/edit/page.js
@@ -13,20 +13,19 @@ export default function EditProduct({ params }) {
 
   useEffect(() => {
     async function fetchData() {
-        try {
-      const data = await ProductData(params.id);
-      setId(data[0].id);
-      setName(data[0].name);
-      setCategory(data[0].category);
-      setImage(data[0].image);
-      setPrice(data[0].price);
-      setDescription(data[0].description);
-      setFeatures(data[0].features);
+      try {
+        const data = await ProductData(params.id);
+        setId(data[0].id);
+        setName(data[0].name);
+        setCategory(data[0].category);
+        setImage(data[0].image);
+        setPrice(data[0].price);
+        setDescription(data[0].description);
+        setFeatures(data[0].features);
+      } catch (error) {
+        console.error("veri getirme hatası:", error.message);
+      }
     }
-    catch (error) {
-        console.error("veri getirme hatası:" , error.message);
-    }
-}
     fetchData();
   }, [params.id]);
 
@@ -59,7 +58,6 @@ export default function EditProduct({ params }) {
     } catch (error) {
       console.log(error.message);
     }
-
   };
 
   return (
@@ -72,34 +70,111 @@ export default function EditProduct({ params }) {
         </h1>
       </div>
       {name ? (
-        <><form>
-                  <img src={image} alt="Ürün fotoğrafı" />
-                  <br />
+        <>
+          <form>
+            <div className="my-4 flex items-center space-x-4 ">
+              <div className="relative w-24 h-24 rounded-full overflow-hidden border-2 border-gray-300">
+                <div className="w-full h-full rounded-full overflow-hidden bg-white">
+                  <img
+                    className="w-full h-full object-cover "
+                    src={image}
+                    alt="Ürün fotoğrafı"
+                  />
+                </div>
+              </div>
 
-                  <label>Ürün Görseli : </label>
-                  <input onChange={(e) => setImage(e.target.value)} value={image} />
-                  <button onClick={handleClickUpdate}>Güncelle</button>
-                  <br />
-                  
-                  <label>İsim :</label>
-                  <input onChange={(e) => setName(e.target.value)} value={name} />
-                  <br />
-                  <label>Kategori :</label>
-                
-                  <input onChange={(e) => setCategory(e.target.value)} value={category} />
-                
-                  <br />
-                  <label>Fiyat : </label>
-                  <input onChange={(e) => setPrice(e.target.value)} value={price} />
-                  <br />
+              <div className="flex flex-col pl-3">
+                <label className="text-gray-700 text-sm font-bold mb-2">
+                  Ürün Görseli:
+                </label>
+                <div className="flex items-center mr-5">
+                  <input
+                    className="w-40 px-2 py-1 border border-gray-300 rounded-l focus:outline-none w-96 mr-3"
+                    onChange={(e) => setImage(e.target.value)}
+                    value={image}
+                  />
+                  <button
+                    className="bg-amber-400 hover:bg-amber-300 text-white font-bold py-1 px-4 rounded-r focus:outline-none"
+                    onClick={handleClickUpdate}
+                  >
+                    Güncelle
+                  </button>
+                </div>
+              </div>
+            </div>
 
-                  <label>Açıklama : </label>
-                  <textarea rows="5" cols="50" onChange={(e) => setDescription(e.target.value)} value={description} />
-                  <br />
-                  <label>Özellikler : </label>
-                  <textarea rows="10" cols="50" onChange={(e) => setFeatures(e.target.value)} value={features} />
-                  <br />
-              </form><button onClick={handleClickUpdate}>Güncelle</button></>
+            <div className="my-4">
+              <label className="block text-gray-700 text-sm font-bold">
+                İsim :
+              </label>
+              <input
+                className="border-2 border-gray-300 rounded p-2 w-full"
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+              />
+              <br />
+            </div>
+
+            <div className="my-4">
+              <label className="block text-gray-700 text-sm font-bold">
+                Kategori :
+              </label>
+
+              <input
+                className="border-2 border-gray-300 rounded p-2 w-full"
+                onChange={(e) => setCategory(e.target.value)}
+                value={category}
+              />
+
+              <br />
+            </div>
+
+            <div className="my-4">
+              <label className="block text-gray-700 text-sm font-bold">
+                Fiyat :{" "}
+              </label>
+              <input
+                className="border-2 border-gray-300 rounded p-2 w-full"
+                onChange={(e) => setPrice(e.target.value)}
+                value={price}
+              />
+              <br />
+            </div>
+            <div className="my-4">
+              <label className="block text-gray-700 text-sm font-bold">
+                Açıklama :{" "}
+              </label>
+              <textarea
+                className="border-2 border-gray-300 rounded p-2 w-full"
+                rows="5"
+                cols="50"
+                onChange={(e) => setDescription(e.target.value)}
+                value={description}
+              />
+              <br />
+            </div>
+
+            <div className="my-4">
+              <label className="block text-gray-700 text-sm font-bold">
+                Özellikler :{" "}
+              </label>
+              <textarea
+                className="border-2 border-gray-300 rounded p-2 w-full"
+                rows="10"
+                cols="50"
+                onChange={(e) => setFeatures(e.target.value)}
+                value={features}
+              />
+              <br />
+            </div>
+          </form>
+          <button
+            className="bg-amber-400 hover:bg-amber-300 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={handleClickUpdate}
+          >
+            Güncelle
+          </button>
+        </>
       ) : (
         <div>Kullanıcı Düzenleme Sayfası Yükleniyor...</div>
       )}
